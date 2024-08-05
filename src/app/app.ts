dc3115c115f2d0fe3b2e8adfdbbc0e6869ec0145
Hao Li <hao.li@valocityglobal.com>
2024-08-05 15:11:00 +1200

    remove light, add get user name

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,13 +10,11 @@ import {
   OpenAIModel,
   PromptManager,
   TurnState,
-  Memory,
   DefaultConversationState
 } from "@microsoft/teams-ai";
 
 // Create AI components
 interface ConversationState extends DefaultConversationState {
-  lightsOn: boolean;
   text: string;
 }
 type ApplicationTurnState = TurnState<ConversationState>;
@@ -49,36 +47,6 @@ const app = new Application<ApplicationTurnState>({
   }
 });
 
-planner.prompts.addFunction('getLightStatus', async (context: TurnContext, memory: Memory) => {
-  return memory.getValue('conversation.lightsOn') ? 'on' : 'off';
-});
-
-app.ai.action('LightsOn', async (context: TurnContext, state: ApplicationTurnState) => {
-  if (state.conversation.lightsOn) {
-    return `the lights are already on`;
-  }
-  state.conversation.lightsOn = true;
-  await context.sendActivity(`[lights on]`);
-  return `the lights are now on`;
-});
-
-app.ai.action('LightsOff', async (context: TurnContext, state: ApplicationTurnState) => {
-  if (!state.conversation.lightsOn) {
-    return `the lights are already off`;
-  }
-  state.conversation.lightsOn = false;
-  await context.sendActivity(`[lights off]`);
-  return `the lights are now off`;
-});
-
-app.ai.action('GetLightStatus', async (context: TurnContext, state: ApplicationTurnState) => {
-  return `the lights are ${state.conversation.lightsOn ? 'on' : 'off'}`;
-});
-
-interface PauseParameters {
-  time: number;
-}
-
 app.ai.action('GetWeather', async (context: TurnContext, state: ApplicationTurnState) => {
   const options = {
     method: 'GET',
@@ -90,16 +58,12 @@ app.ai.action('GetWeather', async (context: TurnContext, state: ApplicationTurnS
     }
   };
     const response = await axios.request(options)
-    state.conversation.text = `The current weather at ${response.data.location.name}, ${response.data.location.country} is ${response.data.current.condition.text}`;
-    await context.sendActivity(`${state.conversation.text}`);
-    context.responded = true;
-    return 'Action done, think about your next action';
+    return 'Here is the data: ' + JSON.stringify(response.data) + 'format this nicely and return it to the user';
 });
 
-app.ai.action('Pause', async (context: TurnContext, state: ApplicationTurnState, parameters: PauseParameters) => {
-  await context.sendActivity(`[pausing for ${parameters.time / 1000} seconds]`);
-  await new Promise((resolve) => setTimeout(resolve, parameters.time));
-  return `done pausing`;
+app.ai.action('GetUsername', async (context: TurnContext, state: ApplicationTurnState) => {
+  await context.sendActivity(`You are ${context.activity.from.name}`);
+  return `user name is ${context.activity.from.name}`;
 });
 
 export default app;
